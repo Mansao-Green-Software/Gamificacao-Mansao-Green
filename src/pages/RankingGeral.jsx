709c4908cb93a4e34be9cb35fcbc2eadcf118043
kevinathy <kevinathy25@gmail.com>
2026-03-20 19:35:08 +0000

    File changes

--- src/pages/RankingGeral.jsx
+++ src/pages/RankingGeral.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { base44 } from "@/api/base44Client";
 import { Trophy, Users, BarChart2 } from "lucide-react";
 
-const SECTORS = ["Social Media", "Audiovisual", "Tráfego", "Líder de Projeto", "Tipster", "Suporte", "Contingência", "Comercial", "Financeiro", "Affiliates", "Administrativo", "Gerência", "Saúde e Bem Estar", "Serviços Gerais", "TV Green", "Feira FC"];
+const SECTORS = ["Administrativo", "Affiliates", "Audiovisual", "Comercial", "Contingência", "Feira FC", "Financeiro", "Gerência", "Líder de Projeto", "Saúde e Bem Estar", "Serviços Gerais", "Social Media", "Suporte", "Tipster", "Tráfego", "TV Green"];
 
 const SECTOR_COLORS = {
   "Social Media": "from-pink-500 to-rose-600",
