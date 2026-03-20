d9b58beec5b59a7f28ed0bb14f1ebdc78c87d7bf
kevinathy <kevinathy25@gmail.com>
2026-03-20 19:55:01 +0000

    File changes

--- src/pages/RankingGeral.jsx
+++ src/pages/RankingGeral.jsx
@@ -72,6 +72,7 @@ export default function RankingGeral() {
   const [loading, setLoading] = useState(true);
 
   const [profiles, setProfiles] = useState({});
+  const [allProfiles, setAllProfiles] = useState([]);
 
   useEffect(() => {
     const load = async () => {
