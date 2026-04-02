2ba863e40d8e7aa9b57c3ce4a2c496e0ad9e0a24
Matheus Mota <matheuzwork@gmail.com>
2026-04-02 17:00:23 -0300

    fix: alteração para cor roxa

--- src/pages/SistemaPontuacao.jsx
+++ src/pages/SistemaPontuacao.jsx
@@ -6,7 +6,7 @@ import { FaRocket, FaClipboardList, FaHeart, FaStar, FaExclamationCircle, FaBull
 const SECTORS = ["Administrativo", "Affiliates", "Audiovisual", "Comercial", "Contingência", "Feira FC", "Financeiro", "Gerência", "IA/Automação", "Líder de Projeto", "Saúde e Bem Estar", "Serviços Gerais", "Social Media", "Suporte", "Supervisor", "TI", "Tipster", "Tráfego", "TV Green"];
 
 const CATEGORIES = [
-  { key: "Performance & Resultados", Icon: FaRocket, color: "text-orange-400 border-orange-700 bg-orange-700" },
+  { key: "Performance & Resultados", Icon: FaRocket, color: "text-purple  -400 border-purple-700 bg-purple-700" },
   { key: "Disciplina & Organização", Icon: FaClipboardList, color: "text-blue-400 border-blue-700 bg-blue-900" },
   { key: "Cultura & Atitude Green", Icon: FaHeart, iconColor: "text-green-400", color: "text-green-400 border-green-700 bg-green-800" },
   { key: "Bônus de Pontuação", Icon: FaStar, color: "text-yellow-400 border-yellow-700 bg-yellow-900" },
